1f7485d6a94067310ebb0943d8c2f997c8ec57ec
17 <ceo.alimov@gmail.com>
2024-02-24 13:53:47 +0500

    Python is trash

--- 1-misol.py
+++ 1-misol.py
@@ -1,6 +1,23 @@
+import os
+os.system("cls")
+
 name = input()
 lastname = input()
 age = int(input())
+if age.is_integer:
+    True
+else:
+    False    
+
 phone = input()
+if phone.startswith("+998"):
+    False
+else: 
+    True
+
 email = input()
 address = input()
+
+
+
+    
